a278d2770ab61ca83babf816750c3b850c8f8d2e
hemughk <45702280+hemughk@users.noreply.github.com>
2023-10-06 07:42:31 +0530

    Update DataSourceConfig.java

--- src/main/java/com/bookstore/config/DataSourceConfig.java
+++ src/main/java/com/bookstore/config/DataSourceConfig.java
@@ -12,7 +12,7 @@ public class DataSourceConfig {
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:mysql://10.128.28.189:3306/mcqdb");
+        dataSourceBuilder.url("jdbc:mysql://10.128.6.186:3306/mcqdb");
         dataSourceBuilder.username("hemanth");
         dataSourceBuilder.password("hemanth");
 
