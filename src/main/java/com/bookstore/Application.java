5aedf8578326085906d5fce7ba17e2d6c36e5fa7
hemughk <45702280+hemughk@users.noreply.github.com>
2024-06-05 10:18:35 +0530

    Update Application.java

--- src/main/java/com/bookstore/Application.java
+++ src/main/java/com/bookstore/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
